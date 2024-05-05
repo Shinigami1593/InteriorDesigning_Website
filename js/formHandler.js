a409ed84458ee5bf6b579015ce74d661d88214e6
Adamya <adamsneupane@gmail.com>
2024-05-05 12:46:02 +0545

    wooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo

--- js/formHandler.js
+++ js/formHandler.js
@@ -22,6 +22,12 @@ function setButtonclickListener() {
     }
     if (input2 === null || input2 === ""){
         alert('Please provide a valid email address')
+    } else {
+        for (i = 0; i<input2.length; i++){
+            if (input2[i] === '@'){
+                
+            }
+        }
     }
     if (input3 === null || input3 === "") {
         alert("Please provide a valid contact number")
